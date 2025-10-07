eb0ddf6ea59b3c7cde3adacc60f86612d0e6537c
Alexander Moreira de Morais <alex.moreira@gmail.com>
2025-10-07 07:49:49 -0300

    Primeiro comit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Espera o documento ser totalmente carregado para executar o script
+$(function(){
+    // Carrega o conteúdo de 'footer.html' no elemento com id 'footer-placeholder'
+    $("#rodape-placeholder").load("includes/rodape.html");
+  });
